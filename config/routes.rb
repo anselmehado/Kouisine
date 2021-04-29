46586f38c17b6263cc014bb1db840d6be76977b9
anselmehado <anselmehado85@yahoo.com>
2021-04-29 18:20:15 +0100

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources  :users
   #resources :users, only: [:new, :create, :show, :destroy, :edit]
 
   resources :posts do
